3e074d3ded96e1f238887d82b6612ded383182da
Brett <ginsutime78@gmail.com>
2021-08-26 15:18:12 -0500

    Importing test sound files and editing of MusicEvent script.

---
+++ Assets/SoundSystem/SOClasses/MusicEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public enum LayerType
+{
+    Additive, //Combine separate tracks together in sequence
+    Single //One at a time, blend or fade to next
+}
+
+[CreateAssetMenu(menuName = "SoundSystem/MusicEvent", fileName = "MUS_")]
+public class MusicEvent : ScriptableObject
+{
+    [SerializeField] AudioClip[] musicLayers;
+    [SerializeField] LayerType layerType = LayerType.Additive;
+    [SerializeField] AudioMixerGroup mixer;
+
+    public AudioClip[] MusicLayers => musicLayers;
+    public LayerType LayerType => layerType;
+    public AudioMixerGroup Mixer => mixer;
+}
